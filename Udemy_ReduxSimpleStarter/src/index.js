fc8d364a7f47a1730fa145e1156c528389c3a26b
Jesse Woo <woojesse@gmail.com>
2016-10-28 17:16:31 -0700

    Started working on Search Bar - class component section Will be using YouTube API

--- Udemy_ReduxSimpleStarter/src/index.js
+++ Udemy_ReduxSimpleStarter/src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
 
-// Create a new REACT component. This component should produce some HTML
-// Declaring a variable, final variable that won't change
-// JSX - dialect of JS, webpack/babel will transform it
-// JSX gets turned into HTML
-// Factory - this is a CLASS
-// ES6 function syntax
+const API_KEY = '';
+
+// Functional Component
 const App = () => {
-	return <div>Hi!</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
-
-// Take this component's generated HTML and put on the DOM
-// We passed a class, not an instance of a component
-// Wrap it with JSX tags to create an instance
-// Need a target container
 ReactDOM.render(<App />, document.querySelector('.container'));

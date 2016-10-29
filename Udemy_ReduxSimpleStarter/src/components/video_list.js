1dc1767d0e8858626447bb3fa80d8e1387c81eee
Jesse Woo <woojesse@gmail.com>
2016-10-28 18:13:09 -0700

    Taking the video prop, returning data

--- Udemy_ReduxSimpleStarter/src/components/video_list.js
+++ Udemy_ReduxSimpleStarter/src/components/video_list.js
@@ -6,11 +6,12 @@ const VideoList = (props) => {
   // array.map(function(number) { return number*2 });
   // [2,4,6]
 
+// Always add a key
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem video={video} />
+    return <VideoListItem key={video.etag} video={video} />
   });
 
-
+  // If you had a stack of cardstock with no index, you would have to update the entire stack. If there is index, you can just update that specific cardstock. Each video has an etag.
   return (
     // Use built in iterators, not 'for' loop
     <ul className="col-md-4 list-group">

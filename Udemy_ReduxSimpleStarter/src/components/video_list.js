1367ca740136da621742418758b6c1771ff4d7c8
Jesse Woo <woojesse@gmail.com>
2016-10-28 17:58:59 -0700

    Using array.map to build out list for JSX

--- Udemy_ReduxSimpleStarter/src/components/video_list.js
+++ Udemy_ReduxSimpleStarter/src/components/video_list.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import VideoListItem from './video_list_item'
 
-// Functional component
-// Those videos props will arrive as arguments
 const VideoList = (props) => {
-  // An array of videos
-  // const videos = props.videos;
+  // var array = [1,2,3];
+  // array.map(function(number) { return number*2 });
+  // [2,4,6]
+
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem video={video} />
+  });
+
+
   return (
+    // Use built in iterators, not 'for' loop
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ul>
   );
 };
--- Udemy_ReduxSimpleStarter/src/index.js
+++ Udemy_ReduxSimpleStarter/src/index.js
@@ -5,22 +5,18 @@ import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 const API_KEY = ''
 
+// Converted into Class Component
 class App extends Component {
 	constructor(props) {
 		super(props);
 
-		// Starts at zero
 		this.state = { videos: [] };
 
-		// Kick off a network request, arbitrary amount of time to get data from the network. In between time it take, rendered videos. 0 => 5 videos
 		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
 			this.setState({ videos });
-			//  this.setState({videos: videos});
 		});
 	}
 
-// Pass data to child component (VideoList) from parent (App
-// Passing props "videos" from YTSearch to VideoList
 	render() {
 		return (
 			<div>

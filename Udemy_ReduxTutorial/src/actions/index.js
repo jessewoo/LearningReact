40a0b22e82a3f01c3a0e59ca801c488fc051dc61
Jesse Woo <woojesse@gmail.com>
2016-10-31 18:36:42 -0700

    Redux Action Creator

---
+++ Udemy_ReduxTutorial/src/actions/index.js
@@ -0,0 +1,4 @@
+// Action Creator, make sure Action Creator is wired up to Redux
+export function selectBook(book) {
+  console.log('A book has been selected', book.title);
+}
--- Udemy_ReduxTutorial/src/containers/book-list.js
+++ Udemy_ReduxTutorial/src/containers/book-list.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 // The glue between Redux and React (not developed in tantem, separate libraries)
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreator } from 'redux';
+
+// Connect action with booklist
 
 // A Container is a component that is aware of the state contained in Redux
 class BookList extends Component {
@@ -33,5 +37,13 @@ function mapStateToProps(state) {
   };
 }
 
+// Anything returned from this function will end up as props on the Booklist container. We are passing in key - selectBook, value - selectBook. Whatever is pass thru, the retun will be available in the container
+function mapDispatchToProps(dispatch) {
+  // Whenever selectBook is called, the result should be passed to all of our reducers. If select Button is click, I want to make sure data is flow thru the dispatch function
+  // Dispatch function receives all the action, spits them all back to reducers
+  return bindActionCreators({selectBook: selectBook},dispatch)
+}
+
+// Promote BookList from a component to a container - it needs to know about this new dispatch method, select Book. Make it available as a prop
 // Connect takes a function and a component, and produces a container
-export default connect(mapStateToProps)(BookList);
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);

44e9c1b9260c896285894f87f49a4bdb863a06c7
Jesse Woo <woojesse@gmail.com>
2016-11-10 16:53:19 -0800

    Search bar for weather app.

--- 3_Weather_Plots/src/containers/search_bar.js
+++ 3_Weather_Plots/src/containers/search_bar.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
 export default class Search extends Component {
+  // Need to construct the state
+  constructor(props) {
+    super(props);
+
+    this.state = {term: '' };
+
+    // Take the input function, bind it to this, replace existing function with it.
+    // Ovewriting locate method
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    // console.log(event.target.value);
+    // 'this' is the INCORRECT context after handoff
+    this.setState({term: event.target.value})
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
+  // If there is a callback that makes a reference to 'this', you need to bind it
+  // Post request - enter > submit the form, normal HTML
   render() {
     return (
-      <form className="input-group">
-        <input />
+      <form className="input-group" onSubmit={this.onFormSubmit}>
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}/>
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>

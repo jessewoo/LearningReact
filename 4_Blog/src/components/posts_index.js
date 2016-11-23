07747c692f0ebf7d067e08120b8ebe4ea3a6fffc
Jesse Woo <woojesse@gmail.com>
2016-11-22 17:12:41 -0800

    componentWillMount - Life cycle method

--- 4_Blog/src/components/posts_index.js
+++ 4_Blog/src/components/posts_index.js
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-  return <div>List of blog posts</div>;
-};
+// Refactor from a functional component to a class based component
+class PostsIndex extends Component {\
+  componentWillMount() {
+    console.log('Good time to call Action Creator to fetch posts')
+  }
+
+
+  render() {
+      return (
+        <div>List of blog posts</div>
+      );
+  }
+}
+
+export default PostsIndex;

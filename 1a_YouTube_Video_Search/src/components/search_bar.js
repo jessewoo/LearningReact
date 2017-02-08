2183b0be3a6ca2d239150a33a30723c097137c80
Jesse Woo <woojesse@gmail.com>
2017-02-08 12:27:58 -0800

    Components

---
+++ 1a_YouTube_Video_Search/src/components/search_bar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// Declares a new class, plain JS object
+class SearchBar extends Component {
+  render() {
+    return <input onChange = {this.onInputChange}/>;
+  }
+
+  onInputChange() {
+
+  }
+
+
+}
+
+export default SearchBar;
--- 1a_YouTube_Video_Search/src/index.js
+++ 1a_YouTube_Video_Search/src/index.js
@@ -1,6 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyDwhFzTsLiaC5jq-C8iVi3g94McwEe5bCc';
+
 // Create a new component. This component should produce some HTML
-const App = function() {
-  return <div>Hi!</div>;
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML, put it on the page (in the DOM)
+// Need to import React Library
+ReactDOM.render(<App />, document.querySelector('.container'));

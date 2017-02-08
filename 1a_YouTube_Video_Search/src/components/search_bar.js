3d1927994e31a0a2fa786866dfb6a433b4d45d4f
Jesse Woo <woojesse@gmail.com>
2017-02-08 15:05:31 -0800

    Setting a state

--- 1a_YouTube_Video_Search/src/components/search_bar.js
+++ 1a_YouTube_Video_Search/src/components/search_bar.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 // Declares a new class, plain JS object
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() {
     return <input onChange = {event => console.log(event.target.value)}/>;
   }

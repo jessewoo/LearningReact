7b0d028403ec4932b3b87d109bf1e499aa0df7e6
Jesse Woo <woojesse@gmail.com>
2017-02-08 10:27:41 -0800

    First commit - Udemy Lectures on ReactJS

---
+++ 1a_YouTube_Video_Search/src/index.js
@@ -0,0 +1,6 @@
+// Create a new component. This component should produce some HTML
+const App = function() {
+  return <div>Hi!</div>;
+}
+
+// Take this component's generated HTML, put it on the page (in the DOM)

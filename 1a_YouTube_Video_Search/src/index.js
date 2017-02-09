3bb8c58c2ff969a1938cd478ec708306bf996232
Jesse Woo <woojesse@gmail.com>
2017-02-09 09:33:27 -0800

    Use of YouTube Search

--- 1a_YouTube_Video_Search/src/index.js
+++ 1a_YouTube_Video_Search/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyDwhFzTsLiaC5jq-C8iVi3g94McwEe5bCc';
+
+YTSearch({key: API_KEY, term: 'golden state warriors'}, function(data) {
+  console.log(data)
+})
 
 // Create a new component. This component should produce some HTML
 // This is a functional component since it doesn't have to deal with STATE

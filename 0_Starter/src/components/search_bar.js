e40f86508e7bc58fb7254ee92b729706a462f2d7
Jesse Woo <woojesse@gmail.com>
2018-01-08 15:42:16 -0800

    Use of state, constructor

--- 0_Starter/src/components/search_bar.js
+++ 0_Starter/src/components/search_bar.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  render() {
-    // return <input onChange={this.onInputChange} />;
-    return <input onChange={event => console.log(event.target.value)} />;
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
   }
 
-  // onInputChange(event) {
-  //   console.log(event.target.value);
-  // }
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+        <br />
+        Value of the input: {this.state.term}
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
--- 0_Starter/src/index.js
+++ 0_Starter/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDwhFzTsLiaC5jq-C8iVi3g94McwEe5bCc';
+
+YTSearch({ key: API_KEY, term: 'steph curry' }, function(data) {
+  console.log(data);
+});
 
 // Create a new component
 const App = () => {

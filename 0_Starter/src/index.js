cc62046dae16f016a7725d8948fa7c245e13bdb6
Jesse Woo <woojesse@gmail.com>
2018-01-08 15:52:43 -0800

    Add in YouTube search API

--- 0_Starter/src/index.js
+++ 0_Starter/src/index.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyDwhFzTsLiaC5jq-C8iVi3g94McwEe5bCc';
 
-YTSearch({ key: API_KEY, term: 'steph curry' }, function(data) {
-  console.log(data);
-});
-
 // Create a new component
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'steph curry' }, videos => {
+      this.setState({ videos });
+      // this.setState({ videos:videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 // Put component's generated HTML into DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
